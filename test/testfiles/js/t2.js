3cf2105ef7644cccfb6a85ad9bfa146c15382bc0
Goun Lee <goun.lee@lge.com>
2019-09-30 19:15:56 +0900

    Support key-value patterh with

---
+++ test/testfiles/js/t2.js
@@ -0,0 +1,19 @@
+var fs = require("foobar");
+var t1 = function() {
+    console.log("If you find this, you found the wrong string!");
+}
+
+t1.prototype.tester = function() {
+    RB.getString("This is a test");
+    rb.getString("This is a test1");
+    rb.getString("This is a test with a unique id", "id1");
+    $L("This is a test with getString Wrapper");
+};
+<SplitItem label={$L('DNS Server')}>{dns || ''}</SplitItem>
+            title: $L('No'),
+<SettingsDivider>{$L({key:'pictureModeSettings', value:'Customize'})}</SettingsDivider>
+        <Container className={css['labeled-picker-item']} role={'region'} aria-label={$L({key:'speaker_channel', value:'Channel'})}>
+            return {
+                'manual': $L('Custom'),
+                'auto': $L({key:'digitalOptionSetByProgram', value:'Set By Program'})
+            }
