32a23053d32e45cd55547c7075339bbb8da2e02a
Goun Lee <goun.lee@lge.com>
2020-01-09 13:57:33 +0900

    Update copyright

--- JavaScriptFile.js
+++ JavaScriptFile.js
@@ -1,7 +1,7 @@
 /*
  * JavaScriptFile.js - plugin to extract resources from a JavaScript source code file
  *
- * Copyright © 2019, JEDLSoft
+ * Copyright © 2019-2020, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
