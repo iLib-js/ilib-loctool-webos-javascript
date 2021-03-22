a17a8d2ffb68110037e4f7b16e1d44254942685d
Goun Lee <goun.lee@lge.com>
2021-03-22 11:15:08 +0900

    Update copyright mark and add update-copyrigyt-years.yml file

--- test/testJavaScriptFile.js
+++ test/testJavaScriptFile.js
@@ -1,7 +1,7 @@
 /*
  * testJavaScriptFile.js - test the JavaScript file handler object.
  *
- * Copyright © 2019-2021, JEDLSoft
+ * Copyright (c) 2019-2021, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- test/testJavaScriptFileType.js
+++ test/testJavaScriptFileType.js
@@ -1,7 +1,7 @@
 /*
  * testJavaScriptFileType.js - test the JavaScript file type handler object.
  *
- * Copyright © 2019-2021, JEDLSoft
+ * Copyright (c) 2019-2021, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
