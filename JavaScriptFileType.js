7db17c4ddb3701255df5a4a738c37053cf64b2e0
Goun Lee <gouni6020@gmail.com>
2021-06-17 13:42:23 +0900

    Update to support current pseudoLocale and pseudoLocales in pseudo branch change

--- JavaScriptFileType.js
+++ JavaScriptFileType.js
@@ -17,7 +17,6 @@
  * limitations under the License.
  */
 
-var fs = require("fs");
 var path = require("path");
 var log4js = require("log4js");
 var logger = log4js.getLogger("loctool.plugin.JavaScriptFileType");
@@ -37,10 +36,21 @@ var JavaScriptFileType = function(project) {
     this.newres = this.API.newTranslationSet(project.getSourceLocale());
     this.pseudo = this.API.newTranslationSet(project.getSourceLocale());
 
-    this.pseudos = {};
-
+    if (project.pseudoLocale && typeof project.pseudoLocale === "string") {
+        project.pseudoLocale = [project.pseudoLocale];
+    }
     // generate all the pseudo bundles we'll need
-    if (project.pseudoLocales && typeof project.pseudoLocales == 'object'){
+    if (project.pseudoLocale && Array.isArray(project.pseudoLocale)) {
+        this.pseudos = {};
+        project.pseudoLocale && project.pseudoLocale.forEach(function(locale) {
+            var pseudo = this.API.getPseudoBundle(locale, this, project);
+            if (pseudo) {
+                this.pseudos[locale] = pseudo;
+            }
+        }.bind(this));
+    }
+    if (project.pseudoLocales && typeof project.pseudoLocales == 'object') {
+        this.pseudos = {};
         for (locale in project.pseudoLocales) {
             var pseudo = this.API.getPseudoBundle(locale, this, project);
             if (pseudo) {
@@ -48,7 +58,6 @@ var JavaScriptFileType = function(project) {
             }
         }
     }
-
     // for use with missing strings
     if (!project.settings.nopseudo) {
         this.missingPseudo = this.API.getPseudoBundle(project.pseudoLocale, this, project);
