2bc92caa0ef5cdee4d5db896a1aecb6b53fea832
Goun Lee <goun.lee@lge.com>
2022-09-22 14:38:53 +0900

    update codes

--- JavaScriptFileType.js
+++ JavaScriptFileType.js
@@ -33,6 +33,7 @@ var JavaScriptFileType = function(project) {
     this.newres = this.API.newTranslationSet(project.getSourceLocale());
     this.pseudo = this.API.newTranslationSet(project.getSourceLocale());
     this.logger = this.API.getLogger("loctool.plugin.webOSJSFileType");
+
     if (project.pseudoLocale && typeof project.pseudoLocale === "string") {
         project.pseudoLocale = [project.pseudoLocale];
     }
@@ -59,26 +60,9 @@ var JavaScriptFileType = function(project) {
     if (!project.settings.nopseudo) {
         this.missingPseudo = this.API.getPseudoBundle(project.pseudoLocale, this, project);
     }
-
-    this.localeMap = Object.assign(overrideBaseLocale, project.settings.localeMap);
 };
 
 var alreadyLocJS = new RegExp(/\.([a-z][a-z](-[A-Z][a-z][a-z][a-z])?(-[A-Z][A-Z](-[A-Z]+)?)?)\.js$/);
-
-var overrideBaseLocale = {
-    "ar-SA": "ar",
-    "ca-AD": "ca",
-    "es-CO": "es",
-    "ku-Arab-IQ": "ku",
-    "sw-Latn-KE": "sw",
-    "ur-UA": "ur",
-    "bs-Latn-BA": "bs",
-    "ha-Latn-NG": "ha",
-    "ky-KG": "ky",
-    "mn-Cyrl-MN": "mn",
-    "sr-Latn-RS": "sr",
-    "pa-IN": "pa"
-}
 
 /**
  * Return true if the given path is a java file and is handled
@@ -134,6 +118,11 @@ JavaScriptFileType.prototype.write = function(translations, locales) {
             return locale !== this.project.sourceLocale && locale !== this.project.pseudoLocale;
         }.bind(this));
 
+
+    if (this.project.settings.localeMap) {
+        Utils.setBaseLocale(this.project.settings.localeMap);
+    }
+
     if (mode === "localize") {
         for (var i = 0; i < resources.length; i++) {
             res = resources[i];
@@ -142,14 +131,8 @@ JavaScriptFileType.prototype.write = function(translations, locales) {
             translationLocales.forEach(function(locale) {
                 this.logger.trace("Localizing JavaScript strings to " + locale);
 
-                if (this.localeMap[locale]){
-                    baseLocale = true;
-                    Utils.setBaseLocale(locale, this.localeMap[locale]);
-                    langDefaultLocale = locale;
-                } else {
-                    baseLocale = Utils.isBaseLocale(locale);
-                    langDefaultLocale = Utils.getBaseLocale(locale);
-                }
+                baseLocale = Utils.isBaseLocale(locale);
+                langDefaultLocale = Utils.getBaseLocale(locale);
                 
                 baseTranslation = res.getSource();
 
