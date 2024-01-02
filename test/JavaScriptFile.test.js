998b7307b409e0137ec9e65f020d15bab6b1609c
Goun Lee <goun.lee@lge.com>
2024-01-02 17:16:47 +0900

    Revert "update"
    This reverts commit 89637b40c7af566dd7a0741ef66fd815b0ca0cdf.

--- test/JavaScriptFile.test.js
+++ test/JavaScriptFile.test.js
@@ -1,7 +1,7 @@
 /*
  * JavaScriptFile.test.js - test the JavaScript file handler object.
  *
- * Copyright (c) 2019-2023, JEDLSoft
+ * Copyright © 2019-2023, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
